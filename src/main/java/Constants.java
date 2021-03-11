c2a375d1fe9b1a2d9d8775546d9beafa7ebe7602
octagonal-t <octagonal.tire@gmail.com>
2021-03-11 18:51:42 -0500

    Changed token to be private

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 public class Constants {
   public final static String prefix = "oct!";
   public final static HashMap<String, BaseCommand> commands = new HashMap<>();
-  public final static String token="NzQxMDIwMTM3NzAzODY2NDE4.XyxeyA.0_cfdoGjEiP5Az2W-CmClc8Ytjs";
+  public final static String token=Token.token;
   public Constants(){
     commands.put("ping", new Ping(commands));
     commands.put("help", new Help(commands));
---
+++ src/main/java/Token.java
@@ -0,0 +1,3 @@
+public class Token {
+  static String token = "NzQxMDIwMTM3NzAzODY2NDE4.XyxeyA.0_cfdoGjEiP5Az2W-CmClc8Ytjs";
+}
