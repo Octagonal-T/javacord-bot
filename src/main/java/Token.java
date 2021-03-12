d001bced3ac4357c16dc325883e69bdfcb660d9b
octagonal-t <octagonal.tire@gmail.com>
2021-03-12 08:50:32 -0500

    Hides the token

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -1,3 +1,3 @@
 public class Token {
-  static String token = "NzQxMDIwMTM3NzAzODY2NDE4.XyxeyA.A65j9Ccj4NQxcqs5KVJoX4Qdhrs";
+  static String token = "NzQxMDIwMTM3NzAzODY2NDE4.XyxeyA.Fr8WdDI-F8-fALv83hJakrGyQA4";
 }
